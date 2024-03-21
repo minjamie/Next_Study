f4cbdffe9603fc52eea04be6ac1dca2c6f0ede66
김민재 <minjae2246@ubcare.co.kr>
2024-03-21 15:16:44 +0900

    동적 경로와 경로 매개변수를 활용한 Meals 세부내용 로딩 및 렌더링 11분

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,32 @@
-export default function MealDetailPage({ params }) {
+import { getMeal } from "@/lib/meal";
+import classes from "./page.module.css";
+import Image from "next/image";
+
+export default async function MealDetailPage({ params }) {
+  const meal = await getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
   return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center" }}>
-        meal {params.slug} Detail
-      </h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill alt="" src={meal.image} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
